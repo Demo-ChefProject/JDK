bd7cdf87f76126562809b2efcd42667563dbe884
Krithi Gnanasekaran <Krithi.Gnanasekaran@nc4.local>
2016-08-24 15:38:28 -0400

    adding knife and node_bootstrap file

--- node_bootstrap.ps1
+++ node_bootstrap.ps1
@@ -1,4 +1,29 @@
 #!/bin/bash
 
 #cd ~/home/jenkins/workspace/Chef-apache/
-cd C:\Jenkins\workspace\Chef-apache
+cd C:\Jenkins\workspace\Chef-Jdk
+
+#sudo knife role from file /home/jenkins/workspace/Chef-apache/roles/apache.rb
+knife role from file C:\Jenkins\workspace\Chef-apache\roles\jdk.json
+
+# Upload cookbooks into Chef Server
+knife upload cookbooks Jdk
+
+# Bootstrap a node to its chef server
+# knife bootstrap windows winrm 54.175.57.21 --winrm-user Administrator --winrm-password 'd*G%tc9"&"HLK' --node-name Rigil_node_Windows -r 'role[apache]' -y
+
+#Passing credentials stored as a secure string
+$Pass = cat C:\securestring.txt | convertto-securestring
+$Pegacred = new-object -TypeName System.Management.Automation.PSCredential -argumentlist "Administrator",$pass
+#  Create a remote session to the chef node
+$Session = New-PSSession -ComputerName 54.175.57.21 -Credential $Pegacred
+
+$Script = {powershell.exe chef-client}
+
+$Job = Invoke-Command -Session $Session -Scriptblock $Script
+echo $Job
+#Script which runs the ruby script in the remote server
+
+
+# Exit and remove the current session
+Remove-PSSession -Session $Session
