050bc464931fdcae5f0e03e08b98e4fdc5130dc6
Krithi Gnanasekaran <Krithi.Gnanasekaran@nc4.local>
2016-08-24 15:19:32 -0400

    initial commit

---
+++ cookbooks/recipes/jdkinstall.rb
@@ -0,0 +1,3 @@
+remote_file 'D:\Program Files' do
+  source 'com/oracle/jdk-64bit/7.101/jdk-64bit-7.101.exe'
+end
---
+++ node_bootstrap.ps1
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#cd ~/home/jenkins/workspace/Chef-apache/
+cd C:\Jenkins\workspace\Chef-apache
