8f0c51a1b048d009810cc13c4e68ae571c13accb
Krithi Gnanasekaran <Krithi.Gnanasekaran@nc4.local>
2016-08-24 17:45:07 -0400

    knife commit

--- knife.rb
+++ knife.rb
@@ -16,7 +16,7 @@
 #krithi knife file
 # See https://docs.getchef.com/config_rb_knife.html for more information on knife configuration options
 
-current_dir = File.dirname(__FILE__)
+#current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
 node_name                "krithi"
