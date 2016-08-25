7680df79929817074418d21e07ca7ec8a4ff432f
Krithi Gnanasekaran <Krithi.Gnanasekaran@nc4.local>
2016-08-25 14:10:02 -0400

    new add

--- cookbooks/Jdk/recipes/jdkinstall.rb
+++ cookbooks/Jdk/recipes/jdkinstall.rb
@@ -2,6 +2,7 @@ remote_file 'C:\Program Files' do
   source 'http://devmcnexus21.nc4.local:8081/#browse/browse/components:maven-releases:e5c50d09b73fd3c5799dbac74dbd21b9:31c950c8eeeab7833f166b43a4c19d6c'
 end
 
+#Test
 #Use of Windows_package for managing MSI packages
 #windows_package 'MSI Install for Jdk'
 
