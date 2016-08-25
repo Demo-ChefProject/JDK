e096af1ef17c56708f851cfa97d2c8c65d755339
Krithi Gnanasekaran <Krithi.Gnanasekaran@nc4.local>
2016-08-25 15:29:08 -0400

    recipe

--- cookbooks/javajdk/recipes/jdkinstall.rb
+++ cookbooks/javajdk/recipes/jdkinstall.rb
@@ -1,3 +1,3 @@
-remote_file 'C:\\Program Files' do
-  source 'http://devmcnexus21.nc4.local:8081/#browse/browse/components:maven-releases:e5c50d09b73fd3c5799dbac74dbd21b9:31c950c8eeeab7833f166b43a4c19d6c'
+remote_file 'C:\Program Files\' do
+  source 'http://devmcnexus21.nc4.local:8081/repository/maven-releases/com/oracle/jdk-64bit/7.101/jdk-64bit-7.101.exe'
 end
