fa8238d0c2b0be34c6127ff4a8f13242a1e90dfd
Krithi Gnanasekaran <Krithi.Gnanasekaran@nc4.local>
2016-08-25 09:08:48 -0400

    trusted cert

--- cookbooks/recipes/jdkinstall.rb
+++ cookbooks/recipes/jdkinstall.rb
@@ -5,3 +5,18 @@ end
 execute "Enable on hosts" do
   command lazy { "#{com/oracle/jdk-64bit/7.101/jdk-64bit-7.101.exe}/command --enable -N #{node['hosts']}" }
 end
+
+
+#Testing
+#JAVA-HOME = // logic to get JAVA-HOME
+#file 'JAVA\JDK\BIN' do
+#  content "JAVA-HOME=#{fqdn}"
+#end
+
+#Followed link https://docs.chef.io/templates.html
+#Testing using template for variable JAVA-HOME
+template 'JAVA\JDK\BIN' do
+  source 'motd.erb'
+  variables  JAVA-HOME
+  action :create
+end
