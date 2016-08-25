922720c47c5be3547bad03a3c513b91c9c04b186
Krithi Gnanasekaran <Krithi.Gnanasekaran@nc4.local>
2016-08-25 10:17:48 -0400

    recipe change

--- cookbooks/recipes/jdkinstall.rb
+++ cookbooks/recipes/jdkinstall.rb
@@ -2,10 +2,8 @@ remote_file 'D:\Program Files' do
   source 'com/oracle/jdk-64bit/7.101/jdk-64bit-7.101.exe'
 end
 
-execute "Enable on hosts" do
-  command lazy { "#{com/oracle/jdk-64bit/7.101/jdk-64bit-7.101.exe}/command --enable -N #{node['hosts']}" }
-end
-
+#Use of Windows_package for managing MSI packages
+windows_package 'MSI Install'
 
 #Testing
 #JAVA-HOME = // logic to get JAVA-HOME
