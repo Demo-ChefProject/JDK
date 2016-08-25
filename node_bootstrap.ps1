107fd096fa834a02bb97dac7cec6bd1ef1814098
Krithi Gnanasekaran <kgnanasekaran@agiletrailblazers.com>
2016-08-25 13:12:21 -0400

    Update node_bootstrap.ps1

--- node_bootstrap.ps1
+++ node_bootstrap.ps1
@@ -15,7 +15,7 @@ knife upload cookbooks Jdk
 
 
 # Bootstrap a node to its chef server
-#knife bootstrap windows winrm 54.175.57.21 --winrm-user Administrator --winrm-password 'd*G%tc9"&"HLK' --node-name Rigil_node_Windows -r 'role[jdkinstall]' -y
+knife bootstrap windows winrm 54.175.57.21 --winrm-user Administrator --winrm-password 'd*G%tc9"&"HLK' --node-name Rigil_node_Windows -r 'role[jdk.json]' -y
 
 #Passing credentials stored as a secure string
 $Pass = cat C:\securestring.txt | convertto-securestring
