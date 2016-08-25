f66de55e3f1111825b06c3161a546db6869ce27d
Krithi Gnanasekaran <Krithi.Gnanasekaran@nc4.local>
2016-08-25 16:32:07 -0400

    file inclusion

--- cookbooks/javajdk/recipes/jdkinstall.rb
+++ cookbooks/javajdk/recipes/jdkinstall.rb
@@ -4,7 +4,7 @@ end
 
 #Test
 #Use of Windows_package for managing MSI packages
-#windows_package 'MSI Install for Jdk'
+windows_package 'MSI Install for Jdk'
 
 powershell_script 'MSI install' do
   #Syntax
@@ -12,4 +12,17 @@ powershell_script 'MSI install' do
     Start-Process C:\Users\krithi.gnanasekaran\Downloads\
     msiexec /i "c:\package.msi" #Syntax Works :)
 
+end
+#Testing
+#JAVA-HOME = // logic to get JAVA-HOME
+#file 'JAVA\JDK\BIN' do
+#  content "JAVA-HOME=#{fqdn}"
+#end
+path = C:\ProgramFiles\Java\jdk1.7.0
+#Followed link https://docs.chef.io/templates.html
+#Testing using template for variable JAVA-HOME
+template 'JAVA\JDK\BIN' do
+  source 'motd.erb'
+  variables  JAVA-HOME
+  action :create
 end
