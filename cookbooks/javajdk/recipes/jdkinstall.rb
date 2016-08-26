6c70a15e5e5ab31f21e9fdd1293e3bbb70dba056
Krithi Gnanasekaran <Krithi.Gnanasekaran@nc4.local>
2016-08-26 17:19:13 -0400

    test

--- cookbooks/javajdk/recipes/jdkinstall.rb
+++ cookbooks/javajdk/recipes/jdkinstall.rb
@@ -17,32 +17,7 @@ end
 powershell_script 'Set JAVA_HOME path' do
   guard_interpreter :powershell_script
   code <<-EOH
-    setx JAVA_HOME "%PATH%;C:\Program Files\Java\jdk1.7.0_101" /M
-    setx JRE_HOME "%PATH%;C:\Program Files\Java\jdk1.7.0_101\jre" /M
+    setx KRITHIJAVA_HOME "%PATH%;C:\Program Files\Java\jdk1.7.0_101" /M
+    setx KRITHIJRE_HOME "%PATH%;C:\Program Files\Java\jdk1.7.0_101\jre" /M
   EOH
 end
-
-
-=begin
-powershell_script 'MSI install' do
-  #Syntax
-  #Start-Process <path>\filename.msi
-    Start-Process C:\Users\krithi.gnanasekaran\Downloads\
-    msiexec /i "c:\package.msi" #Syntax Works:)
-
-
-end
-#Testing
-#JAVA-HOME = // logic to get JAVA-HOME
-#file 'JAVA\JDK\BIN' do
-#  content "JAVA-HOME=#{fqdn}"
-#end
-path = C:\ProgramFiles\Java\jdk1.7.0
-#Followed link https://docs.chef.io/templates.html
-#Testing using template for variable JAVA-HOME
-template 'JAVA\JDK\BIN' do
-  source 'motd.erb'
-  variables  JAVA-HOME
-  action :create
-end
-=end
