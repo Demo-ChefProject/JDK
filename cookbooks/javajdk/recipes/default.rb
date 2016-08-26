e6964288cb4c38f48d69e0f79fa4303a88d8e1aa
Krithi Gnanasekaran <Krithi.Gnanasekaran@nc4.local>
2016-08-26 17:30:38 -0400

    test

--- cookbooks/javajdk/recipes/default.rb
+++ cookbooks/javajdk/recipes/default.rb
@@ -6,24 +6,3 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-
-
-=begin
-#Test
-include_recipe "javajdk"
-
-windows_package 'jdk64bitinstal (en-US)' do
-  source 'http://devmcnexus21.nc4.local:8081/repository/maven-releases/com/oracle/jdk-64bit/7.101/jdk-64bit-7.101.exe'
-  options '-jdk'
-  installer_type :custom
-  action :install
-end
-
-
-windows_package 'Mozilla Firefox 5.0 (x86 en-US)' do
-  source 'http://archive.mozilla.org/pub/mozilla.org/mozilla.org/firefox/releases/5.0/win32/en-US/Firefox%20Setup%205.0.exe'
-  options '-ms'
-  installer_type :custom
-  action :install
-end
-=end
