70898aa5274163a7ff67832a7d6e85537b90dde0
Krithi Gnanasekaran <Krithi.Gnanasekaran@nc4.local>
2016-08-26 18:30:16 -0400

    test

---
+++ cookbooks/javajdk/attributes/default.rb
@@ -0,0 +1,5 @@
+#Variale declaration
+default['nc4']['jdk']['install_location']="C:\Program Files\Java"
+default['nc4']['jdk']['package']="jdk-64bit-7.101.exe"
+default['atb']['nexus']['url']="http://54.175.158.124:8081"
+default['atb']['jdk']['version']="repository/Rigil"
--- cookbooks/javajdk/recipes/jdkinstall.rb
+++ cookbooks/javajdk/recipes/jdkinstall.rb
@@ -1,3 +1,4 @@
+=begin
 #Downloads the jdk file
 remote_file 'C:\Program Files\Java\jdk-64bit-7.101.exe' do
   source 'http://54.175.158.124:8081/repository/Rigil/jdk-64bit-7.101.exe'
@@ -10,6 +11,35 @@ windows_package 'Java jdk-64bit-7.101 install' do
   installer_type :custom
   remote_file_attributes ({
     :path => 'C:\Program Files\Java\jdk-64bit-7.101.exe'
+  })
+end
+
+#Set path
+powershell_script 'Set JAVA_HOME, JRE_HOME path' do
+  guard_interpreter :powershell_script
+  code <<-EOH
+    setx JAVA_HOME "%PATH%;C:\Program Files\Java\jdk1.7.0_101" /M
+    setx JRE_HOME "%PATH%;C:\Program Files\Java\jdk1.7.0_101\jre" /M
+  EOH
+end
+=end
+#Trying with variables
+jdk_install_loc = node['nc4']['jdk']['install_location']
+jdk_package_name = node['nc4']['jdk']['package']
+jdk_download_from = "#{node['atb']['nexus']['url']}/#{node['atb']['jdk']['version']}/#{node['nc4']['jdk']['package']}"
+
+#Downloads the jdk file
+remote_file "#{jdk_install_loc}/#{jdk_package_name}" do
+  source jdk_download_from
+  action :create
+end
+
+#Installs jdk
+windows_package 'Java jdk-64bit-7.101 install' do
+  source jdk_download_from
+  installer_type :custom
+  remote_file_attributes ({
+    :path => "#{jdk_install_loc}/#{jdk_package_name}"
   })
 end
 
