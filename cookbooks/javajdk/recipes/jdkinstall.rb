3ef33f432a31a3bb080c3196f6c9b495f56cc36f
Krithi Gnanasekaran <kgnanasekaran@agiletrailblazers.com>
2016-08-26 09:58:55 -0400

    Update jdkinstall.rb

--- cookbooks/javajdk/recipes/jdkinstall.rb
+++ cookbooks/javajdk/recipes/jdkinstall.rb
@@ -1,4 +1,5 @@
-remote_file 'C:\Program Files\Java ' do
+#remote_file 'C:\Program Files\Java ' do
+  remote_file 'C:\NC4\MC3 ' do
   source 'http://devmcnexus21.nc4.local:8081/repository/maven-releases/com/oracle/jdk-64bit/7.101/jdk-64bit-7.101.exe'
 end
 
