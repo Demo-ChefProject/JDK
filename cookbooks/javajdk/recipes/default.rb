6d7236f6a4c0aca0572e340683d6a784a3ef3a9c
Krithi Gnanasekaran <Krithi.Gnanasekaran@nc4.local>
2016-08-26 11:06:57 -0400

    adding the windows package

--- cookbooks/javajdk/recipes/default.rb
+++ cookbooks/javajdk/recipes/default.rb
@@ -6,5 +6,11 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+include_recipe "jdk64bitinstall"
 
-
+windows_package 'jdk64bitinstal (en-US)' do
+  source 'http://devmcnexus21.nc4.local:8081/repository/maven-releases/com/oracle/jdk-64bit/7.101/jdk-64bit-7.101.exe'
+  options '-jdk'
+  installer_type :custom
+  action :install
+end
