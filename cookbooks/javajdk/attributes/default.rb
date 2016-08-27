47e1693834a8f5cc20d64401d2e24a7f41ed2072
sananda1 <skumar@agiletrailblazers.com>
2016-08-27 12:19:20 -0400

    variablizing everything

--- cookbooks/javajdk/attributes/default.rb
+++ cookbooks/javajdk/attributes/default.rb
@@ -1,5 +1,8 @@
-#Variale declaration
-default['nc4']['jdk']['install_location']="C:\Program Files\Java"
+#Variable definitions for jdk cookbook
+default['nc4']['nexus']['url']="http://54.175.158.124:8081"
+default['nc4']['jdk']['version']="repository/Rigil"
+#default['nc4']['nexus']['url']="http://devmcnexus21.nc4.local:8081/repository/maven-releases"
+#default['nc4']['jdk']['version']="org/apache/apache-httpd-32/2.2.32"
 default['nc4']['jdk']['package']="jdk-64bit-7.101.exe"
-default['atb']['nexus']['url']="http://54.175.158.124:8081"
-default['atb']['jdk']['version']="repository/Rigil"
+
+default['nc4']['jdk']['install_location']="C:\Program Files\Java"
--- cookbooks/javajdk/recipes/jdkinstall.rb
+++ cookbooks/javajdk/recipes/jdkinstall.rb
@@ -1,24 +1,28 @@
+jdk_download_from = "#{node['nc4']['nexus']['url']}/#{node['nc4']['jdk']['version']}/#{node['nc4']['jdk']['package']}"
+jdk_package_name = node['nc4']['jdk']['package']
+jdk_install_loc = node['nc4']['jdk']['install_location']
+
 #Downloads the jdk file
-remote_file 'C:\Program Files\Java\jdk-64bit-7.101.exe' do
-  source 'http://54.175.158.124:8081/repository/Rigil/jdk-64bit-7.101.exe'
+remote_file "#{jdk_install_loc}/#{jdk_package_name}" do
+  source jdk_download_from
   action :create
 end
 
 #Installs jdk
-windows_package 'Java jdk-64bit-7.101 install' do
-  source 'http://54.175.158.124:8081/repository/Rigil/jdk-64bit-7.101.exe'
+windows_package 'Java jdk install' do
+  source jdk_download_from
   installer_type :custom
   remote_file_attributes ({
     :path => 'C:\Program Files\Java\jdk-64bit-7.101.exe'
   })
 end
 
-#Set path
+#Set path variables for Java
 powershell_script 'Set JAVA_HOME, JRE_HOME path' do
   guard_interpreter :powershell_script
   code <<-EOH
-    setx JAVA_HOME "C:\Program Files\Java\jdk1.7.0_101" /M
-    setx JRE_HOME "C:\Program Files\Java\jdk1.7.0_101\jre" /M
+    setx JAVA_HOME "#{jdk_install_loc}\jdk1.7.0_101" /M
+    setx JRE_HOME "#{jdk_install_loc}\jdk1.7.0_101\jre" /M
   EOH
 end
 
