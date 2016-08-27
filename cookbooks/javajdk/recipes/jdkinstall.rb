605c8962c97d17be9bcab0ccf1483321a4835586
Krithi Gnanasekaran <kgnanasekaran@agiletrailblazers.com>
2016-08-27 19:19:43 -0400

    Update jdkinstall.rb

--- cookbooks/javajdk/recipes/jdkinstall.rb
+++ cookbooks/javajdk/recipes/jdkinstall.rb
@@ -23,6 +23,10 @@ windows_package 'Java jdk install' do
   source jdk_download_from
   installer_type :custom
   options "/qn /passive INSTALLDIR=\"#{jdk_inst_folder_name}\""
+  
+  #Testing for checksum
+  checksum shasum -a 256 jdk_inst_folder_name
+  
 #  options "INSTALLDIR=C:\\Java\\Java-JDK-Test"
   #  remote_file_attributes ({
 #    :path => 'C:\Program Files\Java\jdk-64bit-7.101.exe'
