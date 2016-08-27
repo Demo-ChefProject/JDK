5f96747f4c15cc8dd05c8ceb9caaf74ed2455800
sananda1 <skumar@agiletrailblazers.com>
2016-08-27 12:28:44 -0400

    fixing

--- cookbooks/javajdk/attributes/default.rb
+++ cookbooks/javajdk/attributes/default.rb
@@ -5,4 +5,4 @@ default['nc4']['jdk']['version']="repository/Rigil"
 #default['nc4']['jdk']['version']="org/apache/apache-httpd-32/2.2.32"
 default['nc4']['jdk']['package']="jdk-64bit-7.101.exe"
 
-default['nc4']['jdk']['install_location']="C:/Program Files/Java"
+default['nc4']['jdk']['install_location']="C:\\Program Files\\Java"
