2e7f9f133705bd78de55dafdfce17cbd7dd234a5
sananda1 <skumar@agiletrailblazers.com>
2016-08-27 16:44:36 -0400

    fixing errors

--- node_bootstrap.ps1
+++ node_bootstrap.ps1
@@ -18,13 +18,13 @@ knife upload cookbooks javajdk
 #knife bootstrap windows winrm 54.175.57.21 --winrm-user Administrator --winrm-password 'd*G%tc9"&"HLK' --node-name Rigil_node_Windows -r 'role[jdk]' -y
 
 #Passing credentials stored as a secure string
-echo $Pass = cat C:\securestring.txt | convertto-securestring
+$Pass = cat C:\securestring.txt | convertto-securestring
 $Pegacred = new-object -TypeName System.Management.Automation.PSCredential -argumentlist "Administrator",$pass
 #  Create a remote session to the chef node
 $Session = New-PSSession -ComputerName 54.175.57.21 -Credential $Pegacred
 
-$Script = {powershell.exe echo $Install_Folder_Name > jdk_install_name.txt ; powershell.exe chef-client}
-
+#$Script = {powershell.exe echo $Install_Folder_Name > jdk_install_name.txt ; powershell.exe chef-client}
+$Script = {echo $Install_Folder_Name > jdk_install_name.txt ; powershell.exe chef-client}
 $Job = Invoke-Command -Session $Session -Scriptblock $Script
 echo $Job
 #Script which runs the ruby script in the remote server
