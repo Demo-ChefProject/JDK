b5e10528ef0e5229f53014f139b2b3d92898371e
Krithi Gnanasekaran <kgnanasekaran@agiletrailblazers.com>
2016-08-30 09:45:44 -0400

    Update test.rb

--- cookbooks/javajdk/recipes/test.rb
+++ cookbooks/javajdk/recipes/test.rb
@@ -4,7 +4,7 @@
   windows_package 'Java install' do
   source 'http://54.175.158.124:8081repository/Rigil/jdk-64bit-7.101.exe'
   installer_type :custom
-  checksum shasum 256 --check 'C:\Program Files\Java\Testchecksum\jdk-64bit-7.101.exe'
+  shasum -a 256 C:/Program Files/Java/Testchecksum
   remote_file_attributes ({
   :path => 'C:\Program Files\Java\Testchecksum\jdk-64bit-7.101.exe'
   })
