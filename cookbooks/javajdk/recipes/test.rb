c86bef68bc62fe540ec3324b01633e4370750aaf
Krithi Gnanasekaran <kgnanasekaran@agiletrailblazers.com>
2016-08-30 10:19:35 -0400

    Update test.rb

--- cookbooks/javajdk/recipes/test.rb
+++ cookbooks/javajdk/recipes/test.rb
@@ -1,12 +1,12 @@
 
 remote_file 'C:\Program Files\Java\Testchecksum' do
-source 'http://54.175.158.124:8081repository/Rigil/jdk-64bit-7.101.exe'
+source 'http://54.175.158.124:8081/repository/Rigil/jdk-64bit-7.101.exe'
 action :create
 end
   #Testing for checksum funtionality
   
   windows_package 'Java install' do
-  source 'http://54.175.158.124:8081repository/Rigil/jdk-64bit-7.101.exe'
+  source 'http://54.175.158.124:8081/repository/Rigil/jdk-64bit-7.101.exe'
   installer_type :custom
   checksum 'shasum -a 256 C:/Program Files/Java/Testchecksum'
   remote_file_attributes ({
