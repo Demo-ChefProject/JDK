b8cc0c3e3322644ba1718edbbe95de278cd2f78f
Krithi Gnanasekaran <kgnanasekaran@agiletrailblazers.com>
2016-08-31 15:20:57 -0400

    Update node_bootstrap.ps1

--- node_bootstrap.ps1
+++ node_bootstrap.ps1
@@ -7,14 +7,14 @@ knife role from file C:\Jenkins\workspace\Chef-Jdk\roles\jdk.json
 knife upload cookbooks javajdk
 
 # Bootstrap a node to its chef server
-#knife bootstrap windows winrm 54.175.57.21 --winrm-user Administrator --winrm-password 'd*G%tc9"&"HLK' --node-name Rigil_node_Windows -r 'role[jdk]' -y
+#knife bootstrap windows winrm 52.23.166.135 --winrm-user Administrator --winrm-password 'd*G%tc9"&"HLK' --node-name Rigil_node_Windows -r 'role[jdk]' -y
 
 #Passing credentials stored as a secure string
 $Pass = cat C:\securestring.txt | convertto-securestring
 $Pegacred = new-object -TypeName System.Management.Automation.PSCredential -argumentlist "Administrator",$pass
 
 #  Create a remote session to the chef node
-$Session = New-PSSession -ComputerName 54.175.57.21 -Credential $Pegacred
+$Session = New-PSSession -ComputerName 52.23.166.135 -Credential $Pegacred
 
 echo "Preparing to Run....."
 $Script = {powershell.exe chef-client}
